96485beeaac481bc10455e62f7fa1457c4cf02f4
Leevi Kaukonen <leevi.kaukonen@gmail.com>
2024-02-08 12:16:56 +0200

    Added function to liabilities skeleton

--- src/liabilities.py
+++ src/liabilities.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Liabilities:
     def __init__(self, market_rate, guaranteed_rate, util_dependencies):
@@ -7,5 +8,39 @@ class Liabilities:
         self.guaranteed_rate = guaranteed_rate
         self.util_dependencies = util_dependencies
 
-    def mock_liability_method(self):
-        pass
+    def npv_liabilities(rf, rg, discount_rates=None, S = 100000):
+    #rf is a list of the risk free rates
+    #rg is the guaranteed rate
+    
+        T = len(rf)
+        cf = []
+        
+        #calculate the cashflows
+        for t in range(T):
+            r = max(rf[t], rg)
+            
+            if t<(T-1):
+                cf.append(0.01*S) #1% probability of death
+                S = 0.99*S*(1+r)
+            else:
+                cf.append(S)
+        
+        
+        #calculate the discount rates if not provided
+        if discount_rates is not None:
+            d = discount_rates
+        else:
+            d = []
+            for i in range(T):
+                if i < 1:
+                    d_i = 1
+                    d.append(d_i)
+                else:
+                    d_i = d[i-1]/(1+rf[i-1])
+                    d.append(d_i)
+        
+        #print(d)
+        #print(cf)
+        npv = [x * y for x, y in zip(cf, d)]
+        
+        return npv
