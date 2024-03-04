a205e2c7247471b66bc1de2e93f787478d7e5c25
matveizhukov <matzhukov2000@gmail.com>
2024-03-04 17:36:50 +0200

    adding cashflow optimization for a fixed coupon bond

--- src/Products/fixed_rate_coupon.py
+++ src/Products/fixed_rate_coupon.py
@@ -8,7 +8,10 @@ class FixedCouponBond:
         self.discount_rates = discount_rates
         self.S = S
 
-    def calculate_payoff(self, discounted=True):
-        cash_flows = [(self.coupon * self.F) / discount_rate for discount_rate in self.discount_rates] + [self.F / self.discount_rates.iloc[-1]]
+    def calculate_payoff(self, discounted=False):
+        if discounted:
+            cash_flows = [(self.coupon * self.F) / discount_rate for discount_rate in self.discount_rates] + [self.F / self.discount_rates.iloc[-1]]
+        else:
+            cash_flows = [self.coupon * self.F] * len(self.discount_rates) + [self.F]
         return self.S * np.array(cash_flows)
 
--- src/hedging_product.py
+++ src/hedging_product.py
@@ -1,19 +1,18 @@
 from Products.fixed_rate_coupon import FixedCouponBond
 from Products.variable_rate_coupon import VariableCouponBond
 from Products.swaption import Swaption
+import numpy as np
 
 
 class HedgingProduct:
-    def __init__(self, rates, discount_rates):
-        self.rates = rates
-        self.discount_rates = discount_rates
+    def __init__(self):
         self.products = []
 
-    def add_fixed_coupon_bond(self, coupon=0.05, F=100, S=1):
-        bond = FixedCouponBond(coupon, F, self.discount_rates, S)
+    def add_fixed_coupon_bond(self, coupon=0.05, F=1, S=1, discount_rates=np.zeros(60)):
+        bond = FixedCouponBond(coupon, F, discount_rates, S)
         self.products.append(bond)
 
-    def add_variable_coupon_bond(self, F=100, S=1):
+    def add_variable_coupon_bond(self, F=1, S=1):
         bond = VariableCouponBond(self.rates, F, self.discount_rates, S)
         self.products.append(bond)
 
