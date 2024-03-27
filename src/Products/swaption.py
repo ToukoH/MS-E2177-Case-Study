96c871b4f223cfecfcbb999eec334ca6d4a2a3de
vlad.burian <vlad.burian9@gmail.com>
2024-03-27 13:50:20 +0200

    its been one month this is old stuff by now

---
+++ src/Products/swaption.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Swaption:
+    def __init__(self, notional, strike_rate, maturity, underlying_swap_years):
+        self.notional = notional
+        self.strike_rate = strike_rate
+        self.maturity = maturity
+        self.underlying_swap_years = underlying_swap_years
+        self.discount_rates = np.array([])
+        self.forward_rates = np.array([])
+
+    def calculate_payoff(self):
+        """
+        Calculate the payoff of the swaption at expiration.
+        """
+        avg_forward_rate = np.mean(self.forward_rates[:self.underlying_swap_years])
+        value = max(avg_forward_rate - self.strike_rate, 0)
+        payoff = value * self.notional * self.underlying_swap_years
+        return payoff
+
+    def set_discount_rates(self, discount_rates):
+        self.discount_rates = np.array(discount_rates)
+
+    def set_forward_rates(self, forward_rates):
+        self.forward_rates = np.array(forward_rates)
--- src/hedging_strategy.py
+++ src/hedging_strategy.py
@@ -3,14 +3,14 @@ from liabilities import Liabilities
 from hedging_product import HedgingProduct
 from assets import Assets
 from utils import Utils
-
 import pandas as pd
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, LinearConstraint
+
 
 @dataclass
 class HedgingStrategy:
-    def __init__(self, data, guaranteed_rate, S_liabilities):
+    def __init__(self, data, guaranteed_rate, hedging_product: HedgingProduct, S_liabilities):
         """
         data: Data received from Fennia, simulation data only right now
         guaranteed_rate: quaranteed rate
@@ -18,69 +18,73 @@ class HedgingStrategy:
         self.data = data
         self.guaranteed_rate = guaranteed_rate
         self.S_liabilities = S_liabilities
+        self.hp = hedging_product
+        self.products = self.hp.products
 
-        self.npv_asset_liability_list = pd.DataFrame()
 
-    def calculate_npvs(self, S_assets):
-        """
-        S_assets:       size of the asset position
-        """
+    def match_cashflows(self, x):
+        # Need to know the liabilities cashflow!!!
+        liabilities_cashflow = None
+        assets_cashflow = np.multiply(x, np.array([product.calulate_payoff for product in self.products]))
+        return np.linalg.norm(assets_cashflow - liabilities_cashflow)
 
-        assets = Assets(S_assets)
-        liabilities = Liabilities(self.guaranteed_rate)
-        
-        trials = self.data['Trial'].iloc[-1]
-        split_data = np.split(self.data, trials) # Data split into trials, each of which has NPV calculated
-
-        a_npv_list = []
-        l_npv_list = []
-        self.npv_asset_liability_list = pd.DataFrame()
-        
-        for idx, subset in enumerate(split_data): # Loop through the split data
-            market_rates    = subset['SpotRate1'] # Using forecasted spot rates as the market rates
-            discount_rates  = subset['CashTotalReturnIndex']
-
-            pv_liabilities = liabilities.npv_liabilities(market_rates, discount_rates, self.S_liabilities)
-            pv_assets      = assets.npv_assets(market_rates, discount_rates)
-
-            #print(len(pv_assets))
-            
-            #print(len(pv_liabilities))
-            self.npv_asset_liability_list[f'PVs_liabilities_{idx}'] = pv_liabilities
-            self.npv_asset_liability_list[f'PVs_assets_{idx}'] = pv_assets
-            self.npv_asset_liability_list[f'market_rates_{idx}'] = market_rates
-            self.npv_asset_liability_list[f'discount_rates_{idx}'] = discount_rates
-
-            a_npv_list.append(sum(pv_assets))
-            l_npv_list.append(sum(pv_liabilities))
-
-        return a_npv_list, l_npv_list
-    
-    def calculate_means(self, a_npv_list, l_npv_list):
-        """
-        a_npv_list:       list of asset NPVs
-        l_npv_list:       list of liability NPVs
-        """
-
-        asset_npv_mean      = sum(a_npv_list) / len(a_npv_list)
-        liability_npv_mean  = sum(l_npv_list) / len(l_npv_list)
-
-        return asset_npv_mean, liability_npv_mean
-    
-    def match_means(self, S_assets):
-        """
-        S_assets:       size of the asset position
-        """
-                
-        a_npv_list, l_npv_list = self.calculate_npvs(S_assets)
-        asset_npv_mean, liability_npv_mean = self.calculate_means(a_npv_list, l_npv_list)
-        difference = asset_npv_mean - liability_npv_mean
-        print(abs(difference))
-        return abs(difference)
-    
-    def optimize_mean_difference(self, first_guess=10000): 
+    def optimize_mean_difference(self, zero_time_npv):
         print("Optimization started.")
-        # Minimize the difference between mean of assets and mean of liabilities
-        x_0 = [first_guess]
-        res = minimize(self.match_means, x_0, tol=0.5)
-        return res.x[0] # returns the size of the asset portfolio
+        x_0 = np.zeros(len(self.products))
+        x_0 += zero_time_npv/len(x_0)
+        constraint = LinearConstraint(A=np.identity(len(x_0)), lb=zero_time_npv, ub=zero_time_npv)
+        res = minimize(self.match_cashflows, x_0, tol=0.5)
+        return res.x[0]  # returns the size of the asset portfolio
+
+# BELOW NOT IN USE
+
+
+def calculate_npvs(self, S_assets):
+    """
+    S_assets:       size of the asset position
+    """
+
+    assets = Assets(S_assets)
+    liabilities = Liabilities(self.guaranteed_rate)
+
+    trials = self.data['Trial'].iloc[-1]
+    split_data = np.split(self.data, trials)  # Data split into trials, each of which has NPV calculated
+
+    a_npv_list = []
+    l_npv_list = []
+
+    for subset in split_data:  # Loop through the split data
+        market_rates = subset['SpotRate1']  # Using forecasted spot rates as the market rates
+        discount_rates = subset['CashTotalReturnIndex']
+
+        npv_liabilities = liabilities.npv_liabilities(market_rates, discount_rates, self.S_liabilities)
+        npv_assets = assets.npv_assets(market_rates, discount_rates)
+
+        a_npv_list.append(sum(npv_assets))
+        l_npv_list.append(sum(npv_liabilities))
+
+    return a_npv_list, l_npv_list
+
+
+def calculate_means(self, a_npv_list, l_npv_list):
+    """
+    a_npv_list:       list of asset NPVs
+    l_npv_list:       list of liability NPVs
+    """
+
+    asset_npv_mean = sum(a_npv_list) / len(a_npv_list)
+    liability_npv_mean = sum(l_npv_list) / len(l_npv_list)
+
+    return asset_npv_mean, liability_npv_mean
+
+
+def match_means(self, S_assets):
+    """
+    S_assets:       size of the asset position
+    """
+
+    a_npv_list, l_npv_list = self.calculate_npvs(S_assets)
+    asset_npv_mean, liability_npv_mean = self.calculate_means(a_npv_list, l_npv_list)
+    difference = asset_npv_mean - liability_npv_mean
+    print(abs(difference))
+    return abs(difference)
