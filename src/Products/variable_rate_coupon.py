fix bug in payoff calculation

src/Products/variable_rate_coupon.py
@@ -13,7 +13,7 @@ class VariableCouponBond:
     def calculate_payoff(self, t_end, market_rates):
         cashflows = np.zeros(t_end + 1)
         cashflows[self.start_time] = - self.price
-        market_rates_slice = market_rates[self.start_time + 1: self.end_time]
-        cashflows[self.start_time + 1: self.end_time] = self.face_value * market_rates_slice
-        cashflows[self.start_time + self.maturity] = self.face_value + market_rates[self.end_time]
+        market_rates_slice = market_rates[self.start_time + 1: self.end_time + 1]
+        cashflows[self.start_time + 1: self.end_time + 1] = self.face_value * market_rates_slice
+        cashflows[self.start_time + self.maturity] += self.face_value 
         return cashflows