fix npv_liabilities calculation and implement it in optimization

src/contract.py
@@ -60,12 +60,12 @@ class Contract:
         trials = zip(market_rates_rn_list, discount_rates_rn_list)
         accumulated_npvs = 0
 
-        for trial in trials:
-            accumulated_npvs += self.calculate_npv_single_trial(trial)
+        for market_rates_rn, discount_rates_rn in trials:
+            accumulated_npvs += self.calculate_npv_single_trial(market_rates_rn, discount_rates_rn)
 
-        return accumulated_npvs / len(trials)
+        return accumulated_npvs / len(market_rates_rn)
     
-    def calculate_npv_single_trial(self, trial):
+    def calculate_npv_single_trial(self, market_rates_rn, discount_rates_rn):
         """
         This function calculates the NPV of the cashflows for one single trial.
 
@@ -77,8 +77,8 @@ class Contract:
         -------
         NPV of this contract on single trial
         """
-        market_rates_rn, discount_rates_rn = zip(*trial) #unzipping
-        cashflows = self.calculate_cashflows(market_rates_rn)
+        #market_rates_rn, discount_rates_rn = zip(*trial) #unzipping
+        cashflows = self.calculate_cashflows(market_rates_rn[1:])
         pvs = np.zeros(len(market_rates_rn) + 1)
         pvs = cashflows * discount_rates_rn
         return sum(pvs)

src/hedging_strategy.py
@@ -42,10 +42,12 @@ class HedgingStrategy:
         self.market_rates_rn_list = []
         self.discount_factors_rn_list = []
 
-        self.npv_liabilities = 1_000_000_000
 
         self._split_data()
         self._calculate_liab_product_cashflows() # calculate liability and product cashflows only once at the start
+
+        self.npv_liabilities = self.liabilities.calculate_npv(self.market_rates_rn_list, self.discount_factors_rn_list)
+        print(self.npv_liabilities)
 
     def _split_data(self):
         # Real data
@@ -112,7 +114,7 @@ class HedgingStrategy:
         # constraint = LinearConstraint(A=np.identity(len(x_0)), lb=zero_time_npv, ub=zero_time_npv)
         # asset_prices = [product.price for product in self.products] # prices of products could be calculated like this
         asset_prices = np.ones(len(self.products)) # temporary 
-        constraint = ({'type': 'ineq', 'fun': lambda x: self.npv_liabilities - x.dot(asset_prices)}, # npv_liabilities >= assets at t=0
+        constraint = ({'type': 'ineq', 'fun': lambda x: -self.npv_liabilities - x.dot(asset_prices)}, # - npv_liabilities >= assets at t=0 (npv_liab is neg)
                     {'type': 'ineq', 'fun': lambda x: x}) # x >= 0
         bnds = ((0, 1e10) for i in range(len(x_0)))
         res = minimize(self.match_cashflows, x_0, constraints=constraint, tol=0.1)