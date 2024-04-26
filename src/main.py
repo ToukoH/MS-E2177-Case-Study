012f4b6886248128325bda0daaab6de3f99e139c
ToukoH <touko.haapanen@gmail.com>
2024-04-26 15:29:21 +0300

    swap magic

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ data_path_rn = "data/data.csv"
 data_real = data_processing(data_path_real)
 data_rn = data_processing(data_path_rn)
 
-n_contracts = 1
-seed = np.random.default_rng(1) # or 97584730930274884604721697427988122108
+n_contracts = 100
+seed = np.random.default_rng(97584730930274884604721697427988122108) # or 97584730930274884604721697427988122108
 coupon = 0.0
 
 HP = HedgingProduct()
@@ -24,6 +24,7 @@ sizes = seed.integers(1000, 50000, n_contracts)
 
 for i in range(1, 11):
     HP.add_fixed_coupon_bond(maturity=i, coupon = coupon)
+    HP.add_swap(maturity = i, face_value = 1, received_rate = 3.5)
     #HP.add_variable_coupon_bond(maturity=i)
 
 for i in range(n_contracts):
